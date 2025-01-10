059b71064b3205d39295ce43bf7356bb6964afe8
Abdelrahman Abdallah <mabed1227@gmail.com>
2025-01-10 14:00:27 +0200

    halanur customer app

---
+++ lib/appConstants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String app_name = 'halanur';
+  static const String androidPackageName = 'org.halanur.halanur';
+  static const String iosPackageName = 'org.halanur.halanur';
+  static const String baseUrl = 'https://app.halanur.org/app/v1/api/';
+  static const String chatBaseUrl =
+      'https://app.halanur.org/app/v1/Chat_Api/';
+  static const String shareNavigationWebUrl = 'app.halanur.org';
+  static const String defaultLanguage = 'en';
+}
